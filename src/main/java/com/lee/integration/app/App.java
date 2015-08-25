c0be13642c68849e5baf7463b901e680be79d734
Lee <benthal@126.com>
2015-08-26 00:36:40 +0800

    初始化项目

---
+++ src/main/java/com/lee/integration/app/App.java
@@ -0,0 +1,27 @@
+package com.lee.integration.app;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration(exclude={Configuration.class})
+@ImportResource(value={"classpath:spring/root-context.xml"})
+public class App {
+	static final Logger log = LoggerFactory.getLogger(App.class);
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(App.class, args);
+		log.info("Start ... ... OK !");
+	}
+}
